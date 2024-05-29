ba38f17989d2d08138d66181fc691ab85b1c8a73
Тимур Гамахарія <Tymur.Hamakhariia@infiz.khpi.edu.ua>
2024-05-29 15:13:47 +0300

    Добавьте файлы проекта.

---
+++ Gamakharia_IKM722-a_Course_project_new/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gamakharia_IKM722_a_Course_project_new
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
